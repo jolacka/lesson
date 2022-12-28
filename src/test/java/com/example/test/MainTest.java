7747909a0c4d1563a129af902de02bd3714e3e80
paske <paskeviciute@gmail.com>
2022-12-28 20:19:36 +0000

    starting project files

---
+++ src/test/java/com/example/test/MainTest.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testbasicfuncion () {
+        int x = 5;
+        assertEquals (5,x);
+
+    }
+}
