c912f75d41e41cb7720ef9c5d90a44fbe7ca6448
teiserse <teiserse@tcd.ie>
2022-12-28 21:07:58 +0000

    added new function

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(testfunc( 6));
+        System.out.println(newtestfunc( 3));
     }
     public static int testfunc(int x) {
         return (x * 2) + 5;
+
+    }
+    public static int newtestfunc(int x) {
+        return (x * 3) + 5;
 
     }
 }
