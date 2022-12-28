1ac26aaf56a55742e5e055721aa044b47d074a35
paske <paskeviciute@gmail.com>
2022-12-28 20:31:28 +0000

    added test

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(testfunc( 5));
+        System.out.println(testfunc( 6));
     }
     public static int testfunc(int x) {
         return (x * 2) + 5;
